b7104934d9a10ae4786dcc6175a0facbcf9dbf33
Anton Sych <anton.sych.job@gmail.com>
2021-07-29 17:41:17 +0300

    Added handling pagination with Retrofit

--- app/src/main/java/com/archi4zlo/newsapp/models/NewsResponse.kt
+++ app/src/main/java/com/archi4zlo/newsapp/models/NewsResponse.kt
@@ -1,7 +1,7 @@
 package com.archi4zlo.newsapp.models
 
 data class NewsResponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )
--- app/src/main/java/com/archi4zlo/newsapp/util/Constants.kt
+++ app/src/main/java/com/archi4zlo/newsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "2bfbf6c929c14bfc9f01d853e113630c"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
