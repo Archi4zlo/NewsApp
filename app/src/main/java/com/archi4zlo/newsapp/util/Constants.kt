17987ac40ac5b0d842c9c24889a3115f822d757a
Anton Sych <anton.sych.job@gmail.com>
2021-07-29 14:43:45 +0300

    Added search fun to NewsViewModel.kt and chain with SearchNewsFragment.kt

--- app/src/main/java/com/archi4zlo/newsapp/util/Constants.kt
+++ app/src/main/java/com/archi4zlo/newsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "2bfbf6c929c14bfc9f01d853e113630c"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
